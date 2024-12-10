ccb552eedde8f647115d2266132e0bb98e9357a1
Akamundi <145157267+Akamundi@users.noreply.github.com>
2024-12-10 16:02:24 -0500

    Ladder Powerup

--- Assets/Scripts/BlobPowerup.cs
+++ Assets/Scripts/BlobPowerup.cs
@@ -33,10 +33,13 @@ public class BlobPowerup : MonoBehaviour
     public Tilemap tileMap;
     private TilemapCollider2D tileMapCollider;
     private Hole2D currentHole;
-    public GameObject tempTarget;
-
+    public GameObject holeJellybeanTarget;
+    public GameObject ladderJellybeanTarget;
+    public GameObject ladderPrefab;
+    private bool blobTransformed;
     void Start()
     {
+        blobTransformed = false;
         blobPathScript = GetComponent<BlobPath>();
 
         if (tileMap == null)
@@ -60,42 +63,99 @@ public class BlobPowerup : MonoBehaviour
         if (Input.GetMouseButtonDown(0)) // Left mouse button
         {
             UnsummonHole();
+            UnsummonLadder();
+
             GameObject newTarget = new GameObject("Jellybean");
             newTarget.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            CallChase(newTarget);
+            CallChase(newTarget, 0);
+
+        }
+        if (Input.GetMouseButtonDown(1)) // Left mouse button
+        {
+            UnsummonLadder();
+            UnsummonHole();
+
+            GameObject newTarget = new GameObject("Jellybean");
+            newTarget.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            CallChase(newTarget, 1);
 
         }
 
-        if (tempTarget != null)
+        if (holeJellybeanTarget != null && blobTransformed == false)
         {
-            if (Vector2.Distance(transform.position, tempTarget.transform.position) < 1.0f)
+            if (Vector2.Distance(transform.position, holeJellybeanTarget.transform.position) < 1.0f)
+            {
+               // print("success!");
+               
+                
+                SummonHole(holeJellybeanTarget.transform.position + Vector3.down);
+            }
+           // if (blobPathScript.target == blobPathScript.Player.transform) { UnsummonHole(); UnsummonLadder(); holeJellybeanTarget = null; }
+
+        }
+
+        if (ladderJellybeanTarget != null && blobTransformed == false)
+        {
+            if (Vector2.Distance(transform.position, ladderJellybeanTarget.transform.position) < 1.0f)
             {
                 print("success!");
-                UnsummonHole();
-                SummonHole(tempTarget.transform.position + Vector3.down);
+                
+
+
+                SummonLadder(ladderJellybeanTarget.transform.position);
             }
-            if (blobPathScript.target == blobPathScript.Player.transform) { UnsummonHole(); tempTarget = null; }
+           //if (blobPathScript.target == blobPathScript.Player.transform) { UnsummonLadder(); UnsummonHole(); ladderJellybeanTarget = null; }
 
         }
+        if (blobPathScript.target == blobPathScript.Player.transform) { UnsummonLadder(); UnsummonHole(); ladderJellybeanTarget = null; holeJellybeanTarget = null; }
+
+
     }
 
-    void CallChase(GameObject target)
+    void CallChase(GameObject target, int beanType)
     {
 
-        Destroy(tempTarget);
+        Destroy(holeJellybeanTarget);
+        Destroy(ladderJellybeanTarget);
+
         blobPathScript.target = target.transform; 
 
-        tempTarget = target;
+        if (beanType == 0) holeJellybeanTarget = target;
+        else if (beanType == 1) ladderJellybeanTarget = target;
     }
+
+
+    void SummonLadder(Vector3 worldPos) {
+        blobTransformed = true;
+
+        GetComponent<Rigidbody2D>().simulated = false;
+        GetComponent<SpriteRenderer>().enabled = false;
+
+        Vector3Int cellPosition = tileMap.WorldToCell(worldPos);
+
+        Vector3 cellWorldPos = tileMap.CellToWorld(cellPosition);
+
+        Instantiate(ladderPrefab, cellWorldPos + new Vector3(0.0f,ladderPrefab.transform.localScale.y / 2, 0.0f), Quaternion.identity);
+    }
+
+    void UnsummonLadder()
+    {
+        blobTransformed = false;
+
+        GetComponent<Rigidbody2D>().simulated = true;
+        GetComponent<SpriteRenderer>().enabled = true;
+
+        GameObject[] allLadders = GameObject.FindGameObjectsWithTag("Ladder");
+        foreach (GameObject go in allLadders)
+            Destroy(go.transform.parent.gameObject);
+    }
+
 
 
 
     void SummonHole(Vector3 worldPos)
     {
-        GetComponent<Rigidbody2D>().simulated = false;
-        GetComponent<SpriteRenderer>().enabled = false;
-
-
+     
         Vector3Int cellPosition = tileMap.WorldToCell(worldPos);
 
         if (tileMap.HasTile(cellPosition) && !tileMap.HasTile(cellPosition + Vector3Int.down))
@@ -110,10 +170,18 @@ public class BlobPowerup : MonoBehaviour
 
 
             RefreshTileMapCollider();
+
+            GetComponent<Rigidbody2D>().simulated = false;
+            GetComponent<SpriteRenderer>().enabled = false;
+
+            blobTransformed = true;
+
         }
     }
 
     void UnsummonHole() {
+        blobTransformed = false;
+
         GetComponent<Rigidbody2D>().simulated = true;
         GetComponent<SpriteRenderer>().enabled = true;
 
---
+++ Assets/Scripts/LadderMovement.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LadderMovement : MonoBehaviour
+{
+    private float vertical;
+    private float speed = 2f;
+    private bool isLadder;
+    private bool isClimbing;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Collider2D collider2D;
+
+    void Update()
+    {
+        vertical = Input.GetAxisRaw("Vertical");
+
+        if (isLadder)
+        {
+            isClimbing = true;
+            collider2D.isTrigger = true;
+        }
+/*
+        if (Input.GetKeyDown(KeyCode.Space) && isClimbing) {
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.position = new Vector3(transform.position.x - 1, transform.position.y, 0.0f);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.position = new Vector3(transform.position.x + 1, transform.position.y, 0.0f);
+            }
+        }
+*/
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (isClimbing)
+        {
+            rb.gravityScale = 0f;
+            rb.velocity = new Vector2(0, vertical * speed);
+        }
+        else
+        {
+            rb.gravityScale = 1f;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ladder"))
+        {
+            isLadder = true;
+            transform.position = new Vector3(collision.transform.position.x, transform.position.y, 0.0f);
+
+            print("test");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ladder"))
+        {
+            collider2D.isTrigger = false;
+
+            isLadder = false;
+            isClimbing = false;
+        }
+    }
+}
